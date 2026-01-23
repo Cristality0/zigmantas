4d9d5453f69d0618688cf9a31673c968a875b4e9
Cristality_ <jovarasz@gmail.com>
2026-01-23 17:32:42 +0200

    biome lint changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://zigmantas.com',
-	compressHTML: true,
-	build: {
-		inlineStylesheets: 'auto',
-	},
+  site: "https://zigmantas.com",
+  compressHTML: true,
+  build: {
+    inlineStylesheets: "auto",
+  },
 });
--- src/content/config.ts
+++ src/content/config.ts
@@ -1,15 +1,15 @@
-import { defineCollection, z } from 'astro:content';
+import { defineCollection, z } from "astro:content";
 
 const blogCollection = defineCollection({
-	type: 'content',
-	schema: z.object({
-		title: z.string(),
-		description: z.string(),
-		pubDate: z.coerce.date(),
-		author: z.string().default('Jovaras Zigmantas'),
-	}),
+  type: "content",
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    pubDate: z.coerce.date(),
+    author: z.string().default("Jovaras Zigmantas"),
+  }),
 });
 
 export const collections = {
-	blog: blogCollection,
+  blog: blogCollection,
 };
